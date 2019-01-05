751735894c28717252bf8dcb3917505c41c1f19b
Andres Vargas Ramirez <andreselias.vargas@gmail.com>
2019-01-04 23:15:15 -0600

    fix
    Fix Application Design

--- js/controller.js
+++ js/controller.js
@@ -3,6 +3,7 @@
 const btnPlay = document.querySelector('#btnPlay');
 const btnFinish = document.querySelector('#btnFinish');
 const sOutput = document.querySelector('#txtOutputName');
+const selectOptions = document.querySelector('#sectionOptions');
 let sUser = document.querySelector('#txtUserName');
 let sOption = document.querySelector('.option');
 
@@ -30,12 +31,16 @@ const letsPlay = () => {
     let bError = validateUser(sUserName);
 
     if(bError == true){
+
         sOutput.innerHTML = '<p class="text-danger font-weight-bold display-4">User name required</p>';
         btnFinish.disabled = true; //Disable finish button
+
     }else{
+
         sOutput.innerHTML = ''; //erase error
         sUser.disabled = true; //disable input user name
         btnFinish.disabled = false; //enable Finish btn
+        selectOptions.classList.remove('d-none'); //Enable Options
 
 
 
@@ -46,6 +51,8 @@ const letsPlay = () => {
 const finishGame = () => {
     sUser.value = ''; //erase input text value
     sUser.disabled = false; //enable user name
+    btnFinish.disabled = true; //Disable finish button
+    selectOptions.classList.add('d-none'); //Disable Options
 };
 
 btnPlay.addEventListener('click', letsPlay);
