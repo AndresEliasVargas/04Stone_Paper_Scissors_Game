2f7ca821c6ea0c9d9ac95a297bdabccbf320ed04
Andres Vargas Ramirez <andreselias.vargas@gmail.com>
2019-01-05 15:36:20 -0600

    feat
    New features in css, js and html
    New images

--- js/controller.js
+++ js/controller.js
@@ -5,7 +5,8 @@ const btnFinish = document.querySelector('#btnFinish');
 const sOutput = document.querySelector('#txtOutputName');
 const selectOptions = document.querySelector('#sectionOptions');
 let sUser = document.querySelector('#txtUserName');
-let sOption = document.querySelector('.option');
+let sOption = document.querySelector('.imageOption');
+let selectedOption = '';
 
 btnFinish.disabled = true; //Disable finish button
 
@@ -16,9 +17,6 @@ const validateUser = (psUser) => {
     if(psUser === '' || psUser === undefined){
         bError = true;
     };
-    // else if(psOption === '' || psOption === undefined){
-    //     bError = true;
-    // };
 
     return bError;
 
@@ -30,21 +28,21 @@ const letsPlay = () => {
 
     let bError = validateUser(sUserName);
 
+    /* User and finish button */
     if(bError == true){
-
         sOutput.innerHTML = '<p class="text-danger font-weight-bold display-4">User name required</p>';
         btnFinish.disabled = true; //Disable finish button
-
     }else{
-
         sOutput.innerHTML = ''; //erase error
         sUser.disabled = true; //disable input user name
         btnFinish.disabled = false; //enable Finish btn
         selectOptions.classList.remove('d-none'); //Enable Options
-
-
-
     };
+
+    /* Selected option and local storage */
+    
+
+
 
 };
 
--- js/service.js
+++ js/service.js
@@ -1,1 +1,5 @@
 'use strict';
+
+const determineOption = () => {
+    
+}
