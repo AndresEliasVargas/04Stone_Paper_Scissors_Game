5fefd29041aa1ae7f587d0ba3b58bd0bcf7ca0dd
Andres Vargas Ramirez <andreselias.vargas@gmail.com>
2019-01-06 19:48:39 -0600

    fix
    Fixed event click to images

--- js/controller.js
+++ js/controller.js
@@ -3,46 +3,60 @@
 const btnPlay = document.querySelector('#btnPlay');
 const btnFinish = document.querySelector('#btnFinish');
 const sOutput = document.querySelector('#txtOutputName');
-const selectOptions = document.querySelector('#sectionOptions');
+const divSelectOptions = document.querySelector('#sectionOptions');
+const aImages = [document.querySelector('#stone'), document.querySelector('#paper'), document.querySelector('#scissor')];
+const aNames = ['stone', 'paper', 'scissor'];
+
+
 let sUser = document.querySelector('#txtUserName');
-let sOption = document.querySelector('.imageOption');
-let selectedOption = '';
+let sOption = '';
 
 btnFinish.disabled = true; //Disable finish button
 
 const validateUser = (psUser) => {
-
     let bError = false;
 
-    if(psUser === '' || psUser === undefined){
+    if (psUser === '' || psUser === undefined) {
         bError = true;
     };
 
     return bError;
-
 };
 
 const letsPlay = () => {
     let sUserName = sUser.value;
-    //const sOptionUser = sOption.value;
 
     let bError = validateUser(sUserName);
 
     /* User and finish button */
-    if(bError == true){
+    if (bError == true) {
         sOutput.innerHTML = '<p class="text-danger font-weight-bold display-4">User name required</p>';
         btnFinish.disabled = true; //Disable finish button
-    }else{
+    } else {
         sOutput.innerHTML = ''; //erase error
         sUser.disabled = true; //disable input user name
         btnFinish.disabled = false; //enable Finish btn
-        selectOptions.classList.remove('d-none'); //Enable Options
+        divSelectOptions.classList.remove('d-none'); //Enable Options
     };
 
     /* Selected option and local storage */
     
+    divSelectOptions.addEventListener('click', function (event) {
+        let clickedElem = event.target;
+        let selectedOption = 0;
+        
+        //0 = stone || 1 = paper || 2 = scissor 
+        if(clickedElem == aImages[0]){
+            selectedOption = 0;
+        } else if(clickedElem == aImages[1]){
+            selectedOption = 1;
+        } else if(clickedElem == aImages[2]){
+            selectedOption = 2;
+        };
 
-
+        console.log(selectedOption);
+        
+    }, false);
 
 };
 
@@ -50,7 +64,7 @@ const finishGame = () => {
     sUser.value = ''; //erase input text value
     sUser.disabled = false; //enable user name
     btnFinish.disabled = true; //Disable finish button
-    selectOptions.classList.add('d-none'); //Disable Options
+    divSelectOptions.classList.add('d-none'); //Disable Options
 };
 
 btnPlay.addEventListener('click', letsPlay);
--- js/service.js
+++ js/service.js
@@ -1,5 +1,15 @@
 'use strict';
 
-const determineOption = () => {
+// divSelectOptions.addEventListener('click', function (event) {
+//     var clickedElem = event.target;
     
-}
+//     //0 = stone || 1 = paper || 2 = scissor 
+//     if(clickedElem == aImages[0]){
+//         selectedOption = 0;
+//     } else if(clickedElem == aImages[1]){
+//         selectedOption = 1;
+//     } else if(clickedElem == aImages[2]){
+//         selectedOption = 2;
+//     };
+
+// }, false);
